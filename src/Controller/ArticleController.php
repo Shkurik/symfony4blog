03e29f2ec1c71869c28fbb1ad4584b7bac009449
Shkurik <oleksandr.shkuropat@gmail.com>
2019-12-26 18:11:51 +0200

    Implemented Articles (view all, create, update, delete) without comments

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,18 +2,117 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article", name="article")
+     * @Route("/", name="article")
      */
     public function index()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $articles = $em->getRepository(Article::class)->findAll();
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles' => $articles
         ]);
+    }
+
+    /**
+     * @Route("/article/single/{article}", name="single_article")
+     * @param Article $article
+     * @return Response
+     */
+    public function single(Article $article)
+    {
+        return $this->render('article/single.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/create", name="create_article")
+     * @param Request $request
+     * @return RedirectResponse|Response
+     * @throws \Exception
+     */
+    public function create(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->add('submit', SubmitType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $article = $form->getData();
+            $article->setCreatedAt(new \DateTime('now'));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('article');
+        }
+
+        return $this->render('article/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/update{article}", name="update_article")
+     * @param Request $request
+     * @param Article $article
+     * @return RedirectResponse|Response
+     * @throws \Exception
+     */
+    public function update(Request $request, Article $article)
+    {
+        $form = $this->createForm(ArticleType::class, $article, [
+            'action' => $this->generateUrl('update_article', [
+                'article' => $article->getId()
+            ]),
+            'method' => 'POST',
+        ]);
+        $form->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $article = $form->getData();
+            $article->setUpdatedAt(new \DateTime('now'));
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('article');
+        }
+
+        return $this->render('article/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/delete{article}", name="article_delete")
+     * @param Article $article
+     * @return RedirectResponse
+     */
+    public function delete(Article $article)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
+
+        return $this->redirectToRoute('article');
     }
 }
